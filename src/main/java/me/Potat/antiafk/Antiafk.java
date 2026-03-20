2c310bb54409a91d81d98931cbfc9f138d2090fe
Rhidaya Shrestha <rhidayash@gmail.com>
2026-03-20 14:22:29 -0400

    Initial Commit

---
+++ src/main/java/me/Potat/antiafk/Antiafk.java
@@ -0,0 +1,10 @@
+package me.Potat.antiafk;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Antiafk implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
